b5c7d22ebb08466e9e386a704ac629f11af296f4
Damir <damir.urmanov2003@mail.ru>
2022-09-03 21:34:20 +0500

    lesson_5

--- flask_blog/users/utils.py
+++ flask_blog/users/utils.py
@@ -25,7 +25,9 @@ def save_picture(form_picture):
     random_hex = token_hex(8)
     _, file_extension = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + file_extension
-    picture_path = os.path.join(current_app.root_path, "static\\pfps", picture_fn)
+    picture_path = os.path.join(
+        current_app.root_path, "static\\profile_pics", picture_fn
+    )
 
     output_size = (150, 150)
     i = Image.open(form_picture)
