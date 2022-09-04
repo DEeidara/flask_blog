7795e36d7587dace142b3d0c116edf4ac3fc3797
Damir <damir.urmanov2003@mail.ru>
2022-09-04 20:54:57 +0500

    a little fix

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask_blog import init_app, db
+import os
 
 app = init_app()
+file_path = os.path.abspath(os.getcwd()) + "/flask_blog/site.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + file_path
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 db.create_all()
 
 if __name__ == "__main__":
